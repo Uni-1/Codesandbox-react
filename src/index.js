9e3e1d48926e67fe2802c4df95bc0419357ab127
Uni-1 <111950415+Uni-1@users.noreply.github.com>
2022-08-29 10:01:30 +0900

    add

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import ReactDom from "react-dom";
 
 import App from "./App";
 
+const rootElement = document.getElementById("root");
 ReactDom.render(
   <StrictMode>
     <App />
   </StrictMode>,
-  document.getElementById("root")
+  rootElement
+  //document.getElementById("root")
 );
