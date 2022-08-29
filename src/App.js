3794a3979cbff7dbdbbdf9ba91d6ccf9cd25732a
Uni-1 <111950415+Uni-1@users.noreply.github.com>
2022-08-29 10:28:42 +0900

    fix

--- src/App.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDom from "react-dom";
-
-const App = () => {
-  return <h1>Hello!</h1>;
-};
-
-ReactDom.render(<App />, document.getElementById("root"));
--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
-import { StrictMode } from "react";
+import React from "react";
 import ReactDom from "react-dom";
 
-import App from "./App";
+const App = () => {
+  return <h1>Hello!</h1>;
+};
 
-const rootElement = document.getElementById("root");
-ReactDom.render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-  rootElement
-  //document.getElementById("root")
-);
+ReactDom.render(<App />, document.getElementById("root"));
